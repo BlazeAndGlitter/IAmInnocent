b5db1fc53b447e72ca1076799f9405aee5f3484d
Lukesprojectscloud <luke.dahlmanns@web.de>
2025-07-12 08:15:06 +0200

    Dialog system
    nur interrogation szene, keine änderungen in der haupt szene

--- Assets/scripts/DialogSystem.cs
+++ Assets/scripts/DialogSystem.cs
@@ -13,10 +13,10 @@ public class DialogSystem : MonoBehaviour
     public GameObject choice2Button;
 
     [TextArea] public string[] messages; // First two messages
-    [TextArea] public string choice1Result; // Initial response to choice 1
-    [TextArea] public string[] choice1ExtraMessages; // Extra lines after choice 1
-    [TextArea] public string choice2Result; // Initial response to choice 2
-    [TextArea] public string[] choice2ExtraMessages; // Extra lines after choice 2
+    [TextArea] public string[] buttonTextba;
+    [TextArea] public string[] buttonTextbb;
+    public int suspicionrate;
+
     public int currentMessageIndex = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,6 +26,7 @@ public class DialogSystem : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         currentMessageIndex = 0;
         dialogText.text=messages[currentMessageIndex];
+        suspicionrate = 0;
         //zeige ersten Dialog
     }
 
@@ -35,30 +36,86 @@ public class DialogSystem : MonoBehaviour
         //  Cursor.lockState = CursorLockMode.None;
         //Cursor.lockState = CursorLockMode.None;
         //wenn enter pressed, 
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (currentMessageIndex <2)
         {
-            //nextDialog
-            currentMessageIndex++;
-            //zeige aktuellen dialog
-            dialogText.text = messages[currentMessageIndex];
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                //nextDialog
+                currentMessageIndex++;
+                //zeige aktuellen dialog
+                dialogText.text = messages[currentMessageIndex];
+            }
         }
-        if (currentMessageIndex == 1)
+        else if (currentMessageIndex ==2) 
         {
             choice1Button.SetActive(true);
-            choice1Text.text = choice1TextText;
+            choice1Text.text = buttonTextba[currentMessageIndex-2];
             choice2Button.SetActive(true);
-            choice2Text.text = choice2TextText;
+            choice2Text.text = buttonTextbb[currentMessageIndex-2];
+
         }
+        
 
 
+    }
+    public void ShowResult()
+    {
+        choice1Button.SetActive(false);
+        choice2Button.SetActive(false);
+        if (suspicionrate == 0)
+        {
+            dialogText.text = messages[5];
+        }
+        else if (suspicionrate < 3)
+        {
+            dialogText.text = messages[6];
+        }
+        else
+        {
+            dialogText.text = messages[7];
+        }
+    }
+    public void ShowQuestion()
+    {
+        dialogText.text = messages[currentMessageIndex];
+        choice1Button.SetActive(true);
+        choice1Text.text = buttonTextba[currentMessageIndex-2];
+        choice2Button.SetActive(true);
+        choice2Text.text = buttonTextbb[currentMessageIndex-2];
     }
     public void choiceOne()
     {
-        dialogText.text = choice1Result;
-        
+        if (currentMessageIndex < 4)
+        {
+            currentMessageIndex++;
+            ShowQuestion();
+        }
+        else
+        {
+            ShowResult();
+            choice1Button.SetActive(false);
+            choice2Button.SetActive(false);
+        }
+       
     }
     public void choiceTwo()
     {
-        dialogText.text = choice2Result;
+        suspicionrate++;
+
+        if (currentMessageIndex < 4)
+        {
+            currentMessageIndex++;
+            ShowQuestion();
+         
+
+        }
+        else
+        {
+            ShowResult();
+            choice1Button.SetActive(false);
+            choice2Button.SetActive(false);
+        }
     }
+
+
 }
